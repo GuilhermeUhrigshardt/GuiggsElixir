4f5613e901cc929c81d7d489d2f84bba01b8d929
GuilhermeUhrigshardt <guilhermeuh@gmail.com>
2021-04-11 00:32:44 -0300

    Delete apps/data/priv/repo directory

--- apps/data/priv/repo/migrations/20210411001744_add_description_to_events.exs
+++
@@ -1,9 +0,0 @@
-defmodule Data.Repo.Migrations.AddDescriptionToEvents do
-  use Ecto.Migration
-
-  def change do
-    alter table(:events) do
-      add :description, :string, size: 1000
-    end
-  end
-end
